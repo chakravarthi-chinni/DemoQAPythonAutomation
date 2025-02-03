594d48721f443b29469db2555374ede316cf7f07
chakravarthi chinni <chakravarthichinni1@gmail.com>
2025-02-03 12:36:18 +0000

    Edit environment.py

--- featurefiles/environment.py
+++ featurefiles/environment.py
@@ -4,3 +4,5 @@ from BaseFile.basefile import BaseFile
 
 def before_all(context):
     context.base=BaseFile()
+    print(f"DEBUG: context.base type -> {type(context.base)}") 
+    print(f"DEBUG: context.base.driver type -> {type(context.base.driver)}")
