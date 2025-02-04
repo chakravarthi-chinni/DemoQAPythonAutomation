15f69a194f9acaafc027f6d13c3f5bdd6287e00e
chakravarthi chinni <chakravarthichinni1@gmail.com>
2025-02-04 06:27:48 +0000

    Edit basefile.py

--- BaseFile/basefile.py
+++ BaseFile/basefile.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+import logging
 
 
 class BaseFile:
