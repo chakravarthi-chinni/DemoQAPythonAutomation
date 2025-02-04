be1bd8a603b6787e50c8fadbb33f6331e4e86126
chakravarthi chinni <chakravarthichinni1@gmail.com>
2025-02-04 08:45:01 +0000

    Edit basefile.py

--- BaseFile/basefile.py
+++ BaseFile/basefile.py
@@ -17,6 +17,8 @@ class BaseFile:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
 
         try:
             chrome_driver_path = ChromeDriverManager().install()
